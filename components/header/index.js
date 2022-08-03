fa01a9efc084e2ee3e26c8d97b842123ab84e5ca
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-03 20:11:38 -0300

    setting header.js
    setting head change  the Link from div in file page/index.js to dashboard in  file componentes/heaader/index.js

--- components/header/index.js
+++ components/header/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
 
+
 const Header = () => {
 return(
-    <div className= 'bg-slate-800 p-4'>
-    <div className='container mx-auto' >
-       PalpiteBox
+  <React.Fragment>
+    <div className= 'bg-slate-800 p-4 drop-shadow-lg'>
+    <div className='container mx-auto '>
+       <img className='mx-auto' src="/logo_palpitebox.png" height="80" width="80"/>
     </div>
-  
   </div>
+  <div className= 'bg-slate-700 p-4 drop-shadow-lg text-center' >
+            <Link href="/about" >
+                <a className='px-2 hover:underline'>Sobre</a>
+            </Link>
+            
+            <Link href="/contact" >
+                <a className='px-2 hover:underline'>Contato</a>
+            </Link>
+            
+            <Link href="/search" >
+                <a className='px-2 hover:underline'>Pesquisa</a>
+            </Link>
+
+  </div>
+  </React.Fragment>
 )
 }
 export default Header
