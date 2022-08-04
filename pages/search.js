c2b428623dbad0e6809ee4a949382a54974bd38e
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-04 00:08:31 -0300

    setting search page
    setting search.js page set the  first label and input, setting the css

--- pages/search.js
+++ pages/search.js
@@ -3,13 +3,15 @@ import Link from 'next/Link'
 
 const Search = () => {
     return (
-        <div>
-            <h1>Pesquisa</h1>
-            <div>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
+        <div className='pt-6'>
+            <h1 className='text-center font-bold my-4 text-2xl'>Críticas e sugestões</h1>
+            <p className='text-center'>O restaurante X sempre busca por atender melhor seus clientes.<br/>
+            Por isso, estamos sempre abertos a ouvir a sua opiniao.</p>
+            <div className='w-48 mx-auto '>
+                <label className='font-bold ' >Seu nome:</label>
+                <input type='text' className='p-4 block drop-shadow-lg bg-blue-100 m-2 rounded'/>
             </div>
+           
         </div>
     )
 }
